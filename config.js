6d3de0e3fea3e8104174760acf3be6f352d750f3
Missing Handle <missinghandle@gmail.com>
2013-04-12 12:15:48 -0500

    Override CurrentUser atts to be in Config

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 // config.js
 
 var Config = {
-	TriremeURL: "http://165.124.171.122:3347",
-	DynamoXelementClass: "Dynamo.UnitaryXelement"
+	TriremeURL: "http://165.124.171.122:3341",
+	DynamoXelementClass: "Dynamo.UnitaryXelement",
+	CurrentUserGuid: "TEST-USER-GUID",
+	CurrentGroupGuid: "DEMO-GROUP-GUID",
+	CurrentUsername: "GuideCreator"
 }
