c004a23de598910863eb8fc6364fe9e4a19ce25a
Missing Handle <missinghandle@gmail.com>
2013-09-20 18:57:45 -0500

    change trireme to Adult Trial 1

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 
 var Config = {
-	TriremeURL: "http://165.124.171.103:3347",
+	TriremeURL: "http://165.124.171.77:3347",
 	DynamoXelementClass: "Dynamo.UnitaryXelement",
 	CurrentUserGuid: "TEST-USER-GUID",
 	CurrentGroupGuid: "DEMO-GROUP-GUID",
